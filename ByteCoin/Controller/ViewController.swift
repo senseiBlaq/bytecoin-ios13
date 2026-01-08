44d37bec0150b8fc77a2ebe4ed715216a022b7c1
sensei_blaq <jnr.amamoo@gmail.com>
2026-01-08 18:14:53 +0000

    clone app and set for UIPicker datasource and UIPickerView Delegate

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    let coinManger = CoinManager()
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
-
-
 }
 
+
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    
+    /// sets the number of column in the ui picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    /// determines how many rows in the UIPicker
+    /// - Returns: use a method to count the currencies in the array
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManger.currencyArray.count
+    }
+    
+}
+
+
+
+// MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    
+    /// a delegate method that sets the title of th row in the ui picker
+    /// - Returns: the name of the currency got by using the index for currency array
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return coinManger.currencyArray[row]
+    }
+    
+    
+    /// passes the selected currency (row) in the UIPicker to the method in the CoinManager
+   
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        print(row) // debug line
+        let selectedCurrency = coinManger.currencyArray[row]
+        coinManger.getCoinPrice(for: selectedCurrency)
+    }
+}
